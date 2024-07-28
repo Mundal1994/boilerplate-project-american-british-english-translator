70fa763a2b0e7a516a328f8dfe3bc8f9816333db
Mundal1994 <manga_flip@hotmail.com>
2024-07-28 22:55:51 +0300

    finished creating route api/translate and tests/2_functional

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,24 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      let {text, locale} = req.body;
+
+      if (text == null || !locale) {
+        return res.json({ error: 'Required field(s) missing' });
+      }
+
+      if (locale != 'american-to-british' && locale != 'british-to-american') {
+        return res.json({ error: 'Invalid value for locale field' });
+      }
+
+      if (!text.length) {
+        return res.json({ error: 'No text to translate' });
+      }
+
+      const translation = translator.highlight(text, locale == 'american-to-british' ? true : false);
+      return res.json({
+        text: text,
+        translation: translation == text ? 'Everything looks good to me!' : translation
+      })
     });
 };
